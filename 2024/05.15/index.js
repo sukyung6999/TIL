d5d8341606fb387f4b36d539237ca7b16cee570b
sukyung6999 <sukyung6999@gmail.com>
2024-05-15 23:50:39 +0900

    add: 240515 추가

---
+++ 2024/05.15/index.js
@@ -0,0 +1,15 @@
+const { log } = require('console');
+const fs = require('fs');
+let [input, ...coins] = fs.readFileSync("./text.txt").toString().trim().split("\n");
+let [n, total] = input.trim().split(" ");
+
+let count = 0, num = 0;
+coins = coins.map(Number);
+for(let i = n; i >= 0; i--) {
+  if (coins[i] < total) {
+    num = Math.floor(total / coins[i]);
+    count += num;
+    total = total % (coins[i] * num);
+  }
+}
+console.log(count);
